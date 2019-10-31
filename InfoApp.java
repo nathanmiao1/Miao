6b27ea45dbe6e287d24aad0873ef668d9919b269
nathanmiao1 <50683733+nathanmiao1@users.noreply.github.com>
2019-10-31 15:02:14 -0300

    local repository

---
+++ InfoApp.java
@@ -0,0 +1,11 @@
+package cs2043_lab07;
+
+public class InfoApp {
+	
+	public static void main(String[] args) {
+		InfoPrintMiao pr=new InfoPrintMiao();
+		pr.printMiao();
+	}
+// my new work
+
+}
---
+++ InfoPrintMiao.java
@@ -0,0 +1,9 @@
+package cs2043_lab07;
+
+public class InfoPrintMiao {
+	
+	public static void printMiao(){
+		System.out.println("Junwei Miao");
+	}
+// my new work
+}
